a3276daedf4ad23db429570c7b1a7e7c1621d7ed
Benjamin Maitland <marthinwurer@gmail.com>
2019-12-23 15:02:22 -0500

    Added a test to check for nan with the autoencoder

--- test_coordconv_deconv.py
+++ test_coordconv_deconv.py
@@ -3,7 +3,7 @@ import unittest
 import torch
 from torch.nn import functional as F
 
-from single_dot_dataset import CoordConvTranspose2d
+from single_dot_dataset import CoordConvTranspose2d, WMAutoencoder
 from utilities import nan_canary
 
 
@@ -17,10 +17,19 @@ class TestDeconv(unittest.TestCase):
         nan_canary(output)
 
     def test_deconv_nan(self):
-        data = torch.ones((1, 3, 8, 8))#.cuda()
+        data = torch.ones((256, 3, 8, 8))#.cuda()
         mod = CoordConvTranspose2d(3, 50, 3, stride=2)#.cuda()
         output = F.relu(mod(data))
         print(output.shape)
         nan_canary(output)
+
+class TestWMAE(unittest.TestCase):
+    def test_forward(self):
+        batch = torch.rand((32, 3, 64, 64))#.cuda()
+
+        model = WMAutoencoder()#.cuda()
+
+        output, latent = model(batch)
+
 
 
